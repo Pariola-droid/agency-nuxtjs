007af240e7ceec80e03f13009bb73d56a18c8dac
Pariola <kehindephilip15@gmail.com>
2022-05-02 15:44:10 +0100

    ...fixes

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <transition>
+    <header class="header">
+      <div class="header__wrapper">
+          
+      </div>
+    </header>
+  </transition>
+</template>
+
+<script>
+export default {};
+</script>
