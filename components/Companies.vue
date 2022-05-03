97c56a16740459358a4f3b576abb3831610f13f0
Pariola <kehindephilip15@gmail.com>
2022-05-03 09:03:40 +0100

    added:new assets, company component

--- components/Companies.vue
+++ components/Companies.vue
@@ -2,7 +2,20 @@
   <transition>
     <section class="company">
       <div class="company__wrapper">
-        hey
+        <div class="company__wrapper-text">
+          <h3>How 14 Companies Transformed Their Spend Management</h3>
+          <button>Learn More</button>
+        </div>
+        <div class="company__wrapper-spheres">
+          <img
+            class="company__wrapper-spheres-atom"
+            :src="require('../assets/svg/atom.svg')"
+            alt=""
+          />
+          <img :src="require('../assets/svg/sphere-0.svg')" alt="" />
+          <img :src="require('../assets/svg/sphere-1.svg')" alt="" />
+          <img :src="require('../assets/svg/sphere-2.svg')" alt="" />
+        </div>
       </div>
     </section>
   </transition>
