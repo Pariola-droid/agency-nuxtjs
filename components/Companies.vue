d0f8292dc004573864133043218280e2a95e3ebf
Pariola <kehindephilip15@gmail.com>
2022-05-03 09:37:17 +0100

    added: little anime on the company component

--- components/Companies.vue
+++ components/Companies.vue
@@ -15,6 +15,47 @@
           <img :src="require('../assets/svg/sphere-0.svg')" alt="" />
           <img :src="require('../assets/svg/sphere-1.svg')" alt="" />
           <img :src="require('../assets/svg/sphere-2.svg')" alt="" />
+
+          <!-- sphere 2 -->
+          <img
+            class="company__wrapper-spheres-wb"
+            :src="require('../assets/svg/warner bros.svg')"
+            alt=""
+          />
+          <img
+            class="company__wrapper-spheres-cv"
+            :src="require('../assets/svg/canva.svg')"
+            alt=""
+          />
+
+          <!-- sphere 1 -->
+          <img
+            class="company__wrapper-spheres-up"
+            :src="require('../assets/svg/upwork.svg')"
+            alt=""
+          />
+          <img
+            class="company__wrapper-spheres-ab"
+            :src="require('../assets/svg/adobe.svg')"
+            alt=""
+          />
+
+          <!-- sphere 0 -->
+          <img
+            class="company__wrapper-spheres-cc"
+            :src="require('../assets/svg/creative-cloud.svg')"
+            alt=""
+          />
+          <img
+            class="company__wrapper-spheres-mc"
+            :src="require('../assets/svg/mailchimp.svg')"
+            alt=""
+          />
+          <img
+            class="company__wrapper-spheres-gg"
+            :src="require('../assets/svg/google.svg')"
+            alt=""
+          />
         </div>
       </div>
     </section>
